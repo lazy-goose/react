dd64c936c8afb934a9c132713159c80b90e5ea1e
Lazy Goose <shamal.ma.rss@gmail.com>
2023-10-30 11:53:41 +0300

    fix: preprocessing searchPokemons() term with toLowerCase(), trimEnd()

--- react-components/src/API/searchPokemons.ts
+++ react-components/src/API/searchPokemons.ts
@@ -1,6 +1,8 @@
 import { fetchPokemonDescriptions } from './fetchPokemonDescriptions';
 
 export async function searchPokemons(value: string, names: string[]) {
-  const filteredNames = names.filter((v) => v.startsWith(value));
+  const filteredNames = names.filter((v) =>
+    v.startsWith(value.toLowerCase().trimEnd())
+  );
   return await fetchPokemonDescriptions(filteredNames);
 }
