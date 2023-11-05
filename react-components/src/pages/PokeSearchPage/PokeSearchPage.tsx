7b47aeb2708cbd4a7a6fd3b290666d2a18e2c74a
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-05 18:38:58 +0300

    feat: impl pagination using useSearchParams()

--- react-components/src/pages/PokeSearchPage/PokeSearchPage.tsx
+++ react-components/src/pages/PokeSearchPage/PokeSearchPage.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, FormEvent, useEffect, useState, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import s from './PokeSearchPage.module.scss';
 import TextInput from '../../components/@UIKit/TextInput/TextInput';
 import Button from '../../components/@UIKit/Button/Button';
@@ -8,6 +9,7 @@ import PokeList from '../../components/PokeList/PokeList';
 import jcn from '../../utils/joinClassNames';
 import { IPokemon } from 'pokeapi-typescript';
 import { fetchPokemonNames, searchPokemons } from '../../API';
+import Pagination from '../../components/Pagination/Pagination';
 
 const STORAGE_SEARCH = 'pokeSearchString';
 
@@ -18,7 +20,20 @@ export default function PokeSearchPage() {
   const [error, setError] = useState<Error>();
   const [search, setSearch] = useState('');
   const [pokemonRenderArray, setPokemonRenderArray] = useState<IPokemon[]>([]);
-  const [limitRender] = useState(150);
+
+  const DEFAULT_PAGE = 1;
+  const DEFAULT_PAGE_SIZE = 150;
+
+  const [searchParams, setSearchParams] = useSearchParams({
+    page: String(DEFAULT_PAGE),
+    pageSize: String(DEFAULT_PAGE_SIZE),
+  });
+
+  const pageQuery = searchParams.get('page') || '';
+  const pageSizeQuery = searchParams.get('pageSize') || '';
+
+  const page = Number(pageQuery || DEFAULT_PAGE);
+  const pageSize = Number(pageSizeQuery || DEFAULT_PAGE_SIZE);
 
   useEffect(() => {
     const cachedSearchString = localStorage.getItem(STORAGE_SEARCH);
@@ -30,11 +45,14 @@ export default function PokeSearchPage() {
   useEffect(() => {
     (async () => {
       try {
-        pokemonNames.current = await fetchPokemonNames();
+        if (!pokemonNames.current.length) {
+          pokemonNames.current = await fetchPokemonNames();
+        }
         const searchedPokemons = await searchPokemons(
           search,
           pokemonNames.current,
-          limitRender
+          page,
+          pageSize
         );
         setIsFetching(false);
         setPokemonRenderArray(searchedPokemons);
@@ -52,7 +70,8 @@ export default function PokeSearchPage() {
       const searchedPokemons = await searchPokemons(
         search,
         pokemonNames.current,
-        limitRender
+        page,
+        pageSize
       );
       setIsFetching(false);
       setPokemonRenderArray(searchedPokemons);
@@ -64,12 +83,33 @@ export default function PokeSearchPage() {
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setSearch(value);
     localStorage.setItem(STORAGE_SEARCH, value);
+    setSearch(value);
+    setSearchParams((params) => {
+      params.set('page', String(DEFAULT_PAGE));
+      return params;
+    });
   };
 
   const handleErrorButtonClick = () => {
     setError(new Error('No errors occurred? Click here to throw one!'));
+  };
+
+  const handlePageChange = (p: number) => {
+    setSearchParams((params) => {
+      params.set('page', String(p));
+      return params;
+    });
+  };
+
+  const handlePageSizeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setSearchParams((params) => {
+      params.set('pageSize', value.replace(/\D/, ''));
+      params.set('page', String(DEFAULT_PAGE));
+      return params;
+    });
   };
 
   if (error) throw error;
@@ -91,6 +131,26 @@ export default function PokeSearchPage() {
             No errors occurred? Click here to throw one!
           </LinkButton>
         </form>
+        <div>
+          {!isFetching && (
+            <>
+              <Pagination
+                currentPage={page}
+                pageSize={pageSize}
+                totalCount={pokemonNames.current.length}
+                onPageChange={handlePageChange}
+              />
+              <input
+                className={s.PageSizeInput}
+                // Bug with type="number"
+                type="text"
+                value={pageSizeQuery}
+                placeholder={String(DEFAULT_PAGE_SIZE)}
+                onChange={handlePageSizeChange}
+              />
+            </>
+          )}
+        </div>
       </section>
       <section className={jcn(s.BottomSlot)}>
         {isFetching ? <Loader /> : <PokeList pokemons={pokemonRenderArray} />}
