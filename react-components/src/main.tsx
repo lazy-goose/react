e8ec3301b5cb79b3915e5519afa0d30803579367
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-06 02:18:32 +0300

    fix: replace <BrowserRouter /> with <HashRouter />

--- react-components/src/main.tsx
+++ react-components/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import settings from '../settings.json';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename={settings.base}>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </StrictMode>
 );
