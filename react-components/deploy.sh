e2a96bdfe71c8edf4283c9d4dd30d961b7f9c5d2
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-06 01:17:39 +0300

    feat(vite): update deploy script

--- react-components/deploy.sh
+++ react-components/deploy.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+BRANCH_NAME="rect-routing"
+
 npm run build
-git switch gh-pages
+git switch -C gh-pages
 git ls-files . | xargs --no-run-if-empty rm -rf
 cp -R ./dist/* .
 git add .
-git commit -m "deploy(react-components): `date +\"%H:%M %d/%m/%Y\"`"
+git commit -m "deploy($BRANCH_NAME): `date +\"%H:%M %d/%m/%Y\"`"
 git push origin gh-pages
