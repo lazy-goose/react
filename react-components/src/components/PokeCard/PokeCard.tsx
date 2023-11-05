bb75b9f8a3788ec0b6eeeeb5fc771be1705ce516
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-06 01:10:36 +0300

    feat: impl pokemon description sidebar using outlet

--- react-components/src/components/PokeCard/PokeCard.tsx
+++ react-components/src/components/PokeCard/PokeCard.tsx
@@ -1,3 +1,4 @@
+import { NavLink, useSearchParams } from 'react-router-dom';
 import s from './PokeCard.module.scss';
 import jcn from '../../utils/joinClassNames';
 
@@ -11,21 +12,27 @@ type PokeCardProps = {
 
 export default function PokeCard(props: PokeCardProps) {
   const { name, types, imageUrl, imageAlt, className } = props;
+  const [searchParams] = useSearchParams();
   return (
     <div className={jcn(s.PokeCard, className)}>
-      <div className={s.ImageContainer}>
-        {imageUrl ? (
-          <img
-            className={s.Image}
-            src={imageUrl}
-            alt={imageAlt || `${name} pokemon`}
-          />
-        ) : (
-          <div className={s.ImageFallbackBlock}>?</div>
-        )}
-      </div>
-      <h3 className={s.Name}>{name}</h3>
-      <p className={s.Types}>Types: {types.join(', ')}</p>
+      <NavLink
+        to={{ pathname: `pokemon/${name}`, search: searchParams.toString() }}
+        className={s.LinkWrapper}
+      >
+        <div className={s.ImageContainer}>
+          {imageUrl ? (
+            <img
+              className={s.Image}
+              src={imageUrl}
+              alt={imageAlt || `${name} pokemon`}
+            />
+          ) : (
+            <div className={s.ImageFallbackBlock}>?</div>
+          )}
+        </div>
+        <h3 className={s.Name}>{name}</h3>
+        <p className={s.Types}>Types: {types.join(', ')}</p>
+      </NavLink>
     </div>
   );
 }
