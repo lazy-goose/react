a4b5c8a2229473bb737d3461d17a4626db1cb80a
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-06 03:36:02 +0300

    refactor(vite): substitute git branch to deploy commit message

--- react-components/deploy.sh
+++ react-components/deploy.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
-
-set -e
-
-BRANCH_NAME="react-routing"
+#!/bin/sh -e
 
 npm run build
 git switch gh-pages
 git ls-files . | xargs --no-run-if-empty rm -rf
 cp -R ./dist/* .
 git add .
-git commit -m "deploy($BRANCH_NAME): `date +\"%H:%M %d/%m/%Y\"`"
+git commit -m "deploy(`git branch --show-current`): `date +\"%H:%M %d/%m/%Y\"`"
 git push origin gh-pages --force
