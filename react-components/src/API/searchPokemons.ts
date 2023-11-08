7ceabde9cd8fb3abdf4c2c94a18fe46ac55c0359
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-08 11:12:13 +0300

    refactor(api): merge searchPokemons() and fetchPokemonDescriptions()

--- react-components/src/API/searchPokemons.ts
+++ react-components/src/API/searchPokemons.ts
@@ -1,32 +1,35 @@
 import PokeAPI, { IPokemon } from 'pokeapi-typescript';
-
-async function fetchPokemonDescriptions(names: string[]) {
-  const pokemonPromises = names.map((n) => PokeAPI.Pokemon.fetch(n));
-  const settledPokemons = await Promise.allSettled(pokemonPromises);
-  const fulfilledPokemons = settledPokemons.filter(
-    (p) => p.status === 'fulfilled'
-  ) as PromiseFulfilledResult<IPokemon>[];
-  return fulfilledPokemons.map((p) => p.value);
-}
+import { PokemonList } from './fetchPokemonList';
 
 const randomBetween = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 export async function searchPokemons(
   value: string,
-  names: string[],
+  list: PokemonList,
   page = 1,
   limit = 100
 ) {
   const FROM = Math.floor((page - 1) * limit);
   const TO = FROM + limit;
-  const filteredNames = names
-    .filter((v) => v.startsWith(value.toLowerCase().trimEnd()))
+
+  const filteredNames = list
+    .map((i) => i.name)
+    .filter((n) => n.startsWith(value.toLowerCase().trimEnd()))
     .slice(FROM, TO);
+
   if (!filteredNames.length) {
     return new Promise<[]>((resolve) =>
       setTimeout(() => resolve([]), randomBetween(200, 500))
     );
   }
-  return await fetchPokemonDescriptions(filteredNames);
+
+  const descriptionPromises = filteredNames.map((n) =>
+    PokeAPI.Pokemon.resolve(n)
+  );
+  const descriptions = (await Promise.allSettled(descriptionPromises)).filter(
+    (p) => p.status === 'fulfilled'
+  ) as PromiseFulfilledResult<IPokemon>[];
+
+  return descriptions.map((d) => d.value);
 }
