0deb95b0905e0b6a9bcc13d98338f2b991e85c3a
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-08 10:17:45 +0300

    fix: undo outerClick <PokeDetails /> close

--- react-components/src/pages/MainLayout/MainLayout.tsx
+++ react-components/src/pages/MainLayout/MainLayout.tsx
@@ -1,31 +1,21 @@
 import {
   useOutlet,
   useSearchParams,
-  useNavigate,
   NavLink,
 } from 'react-router-dom';
 import PokeSearch from './PokeSearch/PokeSearch';
 import s from './MainLayout.module.scss';
 import PokeDetails from '../../components/PokeDetails/PokeDetails';
-import useOutsideClick from '../../hooks/useOutsideClick';
 
 export default function MainLayout() {
   const outlet = useOutlet();
-  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-
-  const ref = useOutsideClick(() => {
-    navigate({
-      pathname: '/',
-      search: searchParams.toString(),
-    });
-  });
 
   return (
     <main className={s.MainLayout}>
       <PokeSearch />
       {outlet && (
-        <aside ref={ref} className={s.Aside}>
+        <aside className={s.Aside}>
           <div className={s.AsideSticky}>
             <NavLink
               to={{
