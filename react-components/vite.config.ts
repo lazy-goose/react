fb9f487449ddf5ded98860a24c1d34d5322e5535
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-12 20:09:51 +0300

    feat(vite): configure vitest & jsdom & react-testing-library

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import settings from './settings.json'
+import settings from './settings.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: settings.base,
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+  },
 });
