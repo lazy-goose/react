b3c507ccb0f680db3be4e1d5d296adb9ffcb5db7
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-27 01:46:48 +0300

    refactor(tests): migrate 404.test.tsx

--- react-components/src/tests/404.test.tsx
+++ react-components/src/tests/404.test.tsx
@@ -1,6 +1,7 @@
 import { describe, test, expect, beforeEach } from 'vitest';
 import { screen } from '@testing-library/react';
-import renderApp, { mockPokeAPI } from './utils/renderApp';
+import renderComponent, { mockPokeAPI } from './utils/renderComponent';
+import Custom404 from '@/pages/404';
 
 describe('Tests for the 404 Page component', () => {
   beforeEach(() => {
@@ -8,11 +9,9 @@ describe('Tests for the 404 Page component', () => {
   });
 
   test('Ensure that the 404 page is displayed when navigating to an invalid route', async () => {
-    renderApp({ path: '/unknown-page' });
+    renderComponent({ children: <Custom404 />, path: '/unknown-path' });
 
-    const SomethingWrong = await screen.findByText(
-      'Oops... Something went wrong'
-    );
+    const SomethingWrong = await screen.findByText('Page not found');
 
     expect(SomethingWrong).toBeInTheDocument();
   });
