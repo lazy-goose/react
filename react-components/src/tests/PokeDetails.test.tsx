d9cae59279d95ea947d4cefa5683e704227561a9
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-27 01:48:05 +0300

    refactor(tests): migrate PokeDetails.test.tsx

--- react-components/src/tests/PokeDetails.test.tsx
+++ react-components/src/tests/PokeDetails.test.tsx
@@ -1,25 +1,26 @@
-import { describe, test, expect, beforeEach } from 'vitest';
+import { describe, test, expect, beforeEach, vi } from 'vitest';
 import { screen } from '@testing-library/react';
-import renderApp, { mockPokeAPI } from './utils/renderApp';
+import renderComponent, { mockPokeAPI } from './utils/renderComponent';
 import userEvent from '@testing-library/user-event';
 import pokemon from './data/pikachu.json';
+import { MockPokemonRender } from './utils/mockComponents';
+import mockRouter from 'next-router-mock';
 
 describe('Tests for the Detailed Card component', () => {
   const renderPokemonPage = () =>
-    renderApp({ path: `/pokemon/${pokemon.name}` });
+    renderComponent({
+      children: <MockPokemonRender />,
+      path: `/pokemon/${pokemon.name}`,
+    });
 
   beforeEach(() => {
     mockPokeAPI();
   });
 
-  test('Check that a loading indicator is displayed while fetching data', async () => {
+  test('Check that a loading indicator is NOT displayed while fetching data', async () => {
     renderPokemonPage();
     const Loader = screen.queryByTestId('details-loader');
-    expect(Loader).toBeInTheDocument();
-    const Description = await screen.findByTestId(
-      'pokemon-details-description'
-    );
-    expect(Description).toBeInTheDocument();
+    expect(Loader).not.toBeInTheDocument();
   });
 
   test('Make sure the detailed card component correctly displays the detailed card data', async () => {
@@ -31,10 +32,12 @@ describe('Tests for the Detailed Card component', () => {
 
   test('Ensure that clicking the close button hides the component', async () => {
     const user = userEvent.setup();
+    const { push } = mockRouter;
+    mockRouter.push = vi.fn();
     renderPokemonPage();
-    const PokeDetails = await screen.findByTestId('pokemon-details');
     const CloseButton = await screen.findByTestId('close-aside');
     await user.click(CloseButton);
-    expect(PokeDetails).not.toBeInTheDocument();
+    expect(mockRouter.push).toBeCalled();
+    mockRouter.push = push;
   });
 });
