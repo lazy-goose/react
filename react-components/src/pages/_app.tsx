81f52da4021530337ea894eb1869ba7cc0780e0a
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-26 18:44:48 +0300

    fix(next.js): wrap _app with <ErrorBoundary />

--- react-components/src/pages/_app.tsx
+++ react-components/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import '@/share/globals.scss';
+import ErrorBoundary from '@/components/ErrorBoundary';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { setupStore } from '../redux';
 import { Alegreya_Sans } from 'next/font/google';
+import ErrorPage from './error';
 
 const alegreyaSans = Alegreya_Sans({
   weight: ['400', '700'],
@@ -13,10 +15,12 @@ const store = setupStore();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <div className={[alegreyaSans.className, 'app'].join(' ')}>
-        <Component {...pageProps} />
-      </div>
-    </Provider>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <Provider store={store}>
+        <div className={[alegreyaSans.className, 'app'].join(' ')}>
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+    </ErrorBoundary>
   );
 }
--- react-components/src/pages/error.tsx
+++ react-components/src/pages/error.tsx
@@ -1,10 +1,6 @@
-'use client';
-
-import s from './ErrorPage.module.scss';
-
 export default function ErrorPage() {
   return (
-    <div className={s.ErrorPage} style={{ fontSize: '4rem' }}>
+    <div style={{ fontSize: '4rem' }}>
       <p>Oops... Something went wrong</p>
     </div>
   );
