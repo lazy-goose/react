68902c29f448a7dc66bbe8b5f4c37e835db5913c
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-26 18:00:43 +0300

    fix(ui): show fallback on <PokeDetails /> error

--- react-components/src/components/MainLayout/PokeDetails/PokeDetails.tsx
+++ react-components/src/components/MainLayout/PokeDetails/PokeDetails.tsx
@@ -9,62 +9,64 @@ export default function PokeDetails({ pokemonName }: { pokemonName: string }) {
     isLoading,
   } = useGetPokemonByNameQuery(pokemonName);
 
-  if (isError) throw new Error('Jump to ErrorBoundary');
-
   const noDash = (s: string) => s.replaceAll(/[-_]/g, ' ');
 
   return (
     <div className={s.PokeDetails} data-testid="pokemon-details">
-      <h2 className={s.Heading}>{noDash(pokemonName)}</h2>
-      <div className={s.Container}>
-        {isLoading ? (
-          <Loader className={s.Loader} testId="details-loader" />
-        ) : (
-          <ul data-testid="pokemon-details-description">
-            <li>
-              <h4>Weight</h4>
-              <p>{pokemon?.weight}</p>
-            </li>
-            <li>
-              <h4>height</h4>
-              <p>{pokemon?.height}</p>
-            </li>
-            <li>
-              <h4>Base Experience</h4>
-              <p>{pokemon?.base_experience}</p>
-            </li>
-            <li>
-              <h4>Stats</h4>
-              <ul>
-                {pokemon?.stats
-                  .map((s) => [s.stat.name, s.base_stat] as const)
-                  .map(([n, v]) => (
-                    <li key={n} className={s.KeyValue}>
-                      <span>{noDash(n)}</span>
-                      <span>{v}</span>
-                    </li>
-                  ))}
-              </ul>
-            </li>
-            <li>
-              <h4>Abilities</h4>
-              <ul>
-                {pokemon?.abilities
-                  .map((a) => a.ability.name)
-                  .map((n) => <li key={n}>{n}</li>)}
-              </ul>
-            </li>
-            <li>
-              <h4>Types</h4>
-              <ul>
-                {pokemon?.types
-                  .map((t) => t.type.name)
-                  .map((n) => <li key={n}>{n}</li>)}
-              </ul>
-            </li>
-          </ul>
-        )}
-      </div>
+      {isLoading ? (
+        <Loader className={s.Loader} testId="details-loader" />
+      ) : isError ? (
+        <h2 className={s.Heading}>No pokemon found</h2>
+      ) : (
+        <>
+          <h2 className={s.Heading}>{noDash(pokemonName)}</h2>
+          <div className={s.Container}>
+            <ul data-testid="pokemon-details-description">
+              <li>
+                <h4>Weight</h4>
+                <p>{pokemon?.weight}</p>
+              </li>
+              <li>
+                <h4>height</h4>
+                <p>{pokemon?.height}</p>
+              </li>
+              <li>
+                <h4>Base Experience</h4>
+                <p>{pokemon?.base_experience}</p>
+              </li>
+              <li>
+                <h4>Stats</h4>
+                <ul>
+                  {pokemon?.stats
+                    .map((s) => [s.stat.name, s.base_stat] as const)
+                    .map(([n, v]) => (
+                      <li key={n} className={s.KeyValue}>
+                        <span>{noDash(n)}</span>
+                        <span>{v}</span>
+                      </li>
+                    ))}
+                </ul>
+              </li>
+              <li>
+                <h4>Abilities</h4>
+                <ul>
+                  {pokemon?.abilities
+                    .map((a) => a.ability.name)
+                    .map((n) => <li key={n}>{n}</li>)}
+                </ul>
+              </li>
+              <li>
+                <h4>Types</h4>
+                <ul>
+                  {pokemon?.types
+                    .map((t) => t.type.name)
+                    .map((n) => <li key={n}>{n}</li>)}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
