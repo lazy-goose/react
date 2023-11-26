357cccb73e15aa9b52f5c64823a9d9b349624430
Lazy Goose <shamal.ma.rss@gmail.com>
2023-11-27 01:48:40 +0300

    refactor(tests): migrate PokeList.test.tsx

--- react-components/src/tests/PokeList.test.tsx
+++ react-components/src/tests/PokeList.test.tsx
@@ -1,26 +1,20 @@
 import { describe, test, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { IPokemon } from 'pokeapi-typescript';
-import { MemoryRouter } from 'react-router';
-import PokeList from '../components/PokeList/PokeList';
+import renderComponent from './utils/renderComponent';
 import stubPokemons from './data/pokemons.json';
+import PokeList from '@/components/PokeList/PokeList';
 
 describe('Tests for the Card List component', () => {
-  const renderPokeList = (pokemons: IPokemon[]) =>
-    render(
-      <MemoryRouter>
-        <PokeList pokemons={pokemons} />
-      </MemoryRouter>
-    );
-
   test('Verify that the component renders the specified number of cards', async () => {
-    renderPokeList(stubPokemons as IPokemon[]);
+    const pokemons = stubPokemons as IPokemon[];
+    renderComponent({ children: <PokeList pokemons={pokemons} /> });
     const PokemonsContainer = await screen.findByTestId('pokemons-container');
     expect(PokemonsContainer.children).toHaveLength(stubPokemons.length);
   });
 
   test('Check that an appropriate message is displayed if no cards are present', () => {
-    renderPokeList([]);
+    renderComponent({ children: <PokeList pokemons={[]} /> });
     const NotFound = screen.getByText('No pokemon found');
     expect(NotFound).toBeInTheDocument();
   });
