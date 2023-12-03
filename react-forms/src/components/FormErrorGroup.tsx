9acc6b1a70c74cff6d3545a84846bb5b25ec842c
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-03 13:44:48 +0300

    feat: impl <FormErrorGroup />

---
+++ react-forms/src/components/FormErrorGroup.tsx
@@ -0,0 +1,24 @@
+import { type DetailedHTMLProps, type HTMLAttributes } from 'react';
+
+type Props = DetailedHTMLProps<
+  HTMLAttributes<HTMLFieldSetElement>,
+  HTMLFieldSetElement
+> & {
+  legend?: string;
+  error?: string;
+};
+
+function FormErrorGroup(props: Props) {
+  const { children, legend, error = null, ...fieldsetPass } = props;
+  const { className, ...passFieldsetProps } = fieldsetPass;
+  const joinedClassName = ['formErrorGroup', className].join(' ');
+  return (
+    <fieldset className={joinedClassName} {...passFieldsetProps}>
+      <legend>{legend}</legend>
+      {children}
+      <span>{error}</span>
+    </fieldset>
+  );
+}
+
+export default FormErrorGroup;
